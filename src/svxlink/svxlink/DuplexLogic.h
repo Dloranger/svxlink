6eb772f5894e4173a86e5aa2d55d82df059f53d7
Richard Neese <r.neese@gmail.com>
2018-02-07 09:02:29 -0500

    fixing build Repeater_logic to Duplex_Logic

--- src/svxlink/svxlink/DuplexLogic.h
+++ src/svxlink/svxlink/DuplexLogic.h
@@ -25,8 +25,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 
-#ifndef REPEATER_LOGIC_INCLUDED
-#define REPEATER_LOGIC_INCLUDED
+#ifndef DUPLEX_LOGIC_INCLUDED
+#define DUPLEX_LOGIC_INCLUDED
 
 
 /****************************************************************************
